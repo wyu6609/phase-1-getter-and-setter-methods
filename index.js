95effa9412d8b0b35855fd78ff6f835d7e6a65fd
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-19 00:01:49 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  //diameter
+  get diameter() {
+    return 2 * this.radius;
+  }
+  set diameter(newDiameter) {
+    this.radius = newDiameter / 2;
+  }
+
+  //circumference
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+  set circumference(newCircumference) {
+    this.radius = newCircumference / (2 * Math.PI);
+  }
+
+  //area
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
+  set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI);
+  }
+}
